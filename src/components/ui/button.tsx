b7ed11a2c3f9210061fa0c99908a3f6d796e9a6c
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-04-22 00:02:15 +0500

    error resolved and favicon added

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,12 +4,12 @@ import { FC, ReactNode } from "react";
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  type?: string;
+  type?: any;
 }
 
 const Button: FC<ButtonProps> = ({ children, className, type }) => {
   return (
-    <button className={`${className} group`} type={type as any}>
+    <button className={`${className} group`} type={type}>
       {children}
       <ArrowUpRight className="group-hover:rotate-90 transition-transform duration-300" />
     </button>
