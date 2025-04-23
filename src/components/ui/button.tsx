4119568c2c55b99bb626909ea5ff073bdcacc4a4
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-04-23 22:20:00 +0500

    type any error resolved

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,24 +7,15 @@ interface ButtonProps {
   children: ReactNode;
   className?: string;
   type?: ButtonType;
-  disabled?: any;
-  onClick?: any;
 }
 
 const Button: FC<ButtonProps> = ({
   children,
   className = "",
   type = "button",
-  disabled,
-  onClick,
 }) => {
   return (
-    <button
-      className={`${className} group`}
-      type={type}
-      disabled={disabled}
-      onClick={onClick}
-    >
+    <button className={`${className} group`} type={type}>
       {children}
       <ArrowUpRight className="group-hover:rotate-90 transition-transform duration-300" />
     </button>
